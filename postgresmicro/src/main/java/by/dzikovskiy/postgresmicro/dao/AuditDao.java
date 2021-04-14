eaf7cef67cb89ff613fe40c0ec4bd044f7118620
Vitali <vital.dz@mail.ru>
2021-04-14 17:48:12 +0300

    migration to yaml and sql changelogs
    migration to yaml and sql changelogs

--- postgresmicro/src/main/java/by/dzikovskiy/postgresmicro/dao/AuditDao.java
+++ postgresmicro/src/main/java/by/dzikovskiy/postgresmicro/dao/AuditDao.java
@@ -4,7 +4,11 @@ import by.dzikovskiy.postgresmicro.dto.AuditDto;
 import by.dzikovskiy.postgresmicro.mapper.AuditDtoMapper;
 import by.dzikovskiy.postgresmicro.repository.AuditRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -13,7 +17,10 @@ public class AuditDao {
     private final AuditDtoMapper auditDtoMapper;
 
     public AuditDto save(AuditDto auditDto) {
-        return auditDtoMapper.auditToAuditDto(auditRepository.save(auditDtoMapper.auditDtoToAudit(auditDto)));
+        return Optional.ofNullable(auditDto)
+                .map(auditDtoMapper::auditDtoToAudit)
+                .map(auditRepository::save)
+                .map(auditDtoMapper::auditToAuditDto)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST));
     }
-
 }
