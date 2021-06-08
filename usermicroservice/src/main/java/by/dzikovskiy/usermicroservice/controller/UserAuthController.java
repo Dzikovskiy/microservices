af880f877a5beffc57f2c4375a2ddfe2cb43b30a
Vitali <vital.dz@mail.ru>
2021-06-08 11:58:14 +0300

    token refresh
    token refresh

--- usermicroservice/src/main/java/by/dzikovskiy/usermicroservice/controller/UserAuthController.java
+++ usermicroservice/src/main/java/by/dzikovskiy/usermicroservice/controller/UserAuthController.java
@@ -1,11 +1,17 @@
 package by.dzikovskiy.usermicroservice.controller;
 
 
+import by.dzikovskiy.usermicroservice.security.entity.RefreshToken;
 import by.dzikovskiy.usermicroservice.security.entity.UserAuth;
 import by.dzikovskiy.usermicroservice.security.entity.request.AuthRequest;
 import by.dzikovskiy.usermicroservice.security.entity.request.RegistrationRequest;
+import by.dzikovskiy.usermicroservice.security.entity.request.TokenRefreshRequest;
 import by.dzikovskiy.usermicroservice.security.entity.response.AuthResponse;
+import by.dzikovskiy.usermicroservice.security.exception.TokenRefreshException;
 import by.dzikovskiy.usermicroservice.security.service.JwtUtils;
+import by.dzikovskiy.usermicroservice.security.service.RefreshTokenService;
+import by.dzikovskiy.usermicroservice.security.service.UserDetailsImpl;
+import by.dzikovskiy.usermicroservice.security.service.UserDetailsServiceImpl;
 import by.dzikovskiy.usermicroservice.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -26,6 +32,8 @@ public class UserAuthController {
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
     private final JwtUtils jwtUtils;
+    private final RefreshTokenService refreshTokenService;
+    private final UserDetailsServiceImpl userDetailsService;
 
     @PostMapping("/register")
     public ResponseEntity<HttpStatus> registerUser(@RequestBody @Valid RegistrationRequest registrationRequest) {
@@ -44,9 +52,27 @@ public class UserAuthController {
                 new UsernamePasswordAuthenticationToken(loginRequest.getLogin(), loginRequest.getPassword()));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = jwtUtils.generateJwtToken(authentication);
+        UserDetailsImpl userDetails = (UserDetailsImpl) userDetailsService.loadUserByUsername(loginRequest.getLogin());
+        String jwt = jwtUtils.generateJwtToken(userDetails.getUsername());
 
-        return ResponseEntity.ok(new AuthResponse(jwt));
+        RefreshToken refreshToken = refreshTokenService.createRefreshToken(userDetails.getId());
+
+        return ResponseEntity.ok(new AuthResponse(jwt, refreshToken.getToken()));
+    }
+
+    @PostMapping("/refreshtoken")
+    public ResponseEntity<?> refreshToken(@Valid @RequestBody TokenRefreshRequest request) {
+        String requestRefreshToken = request.getRefreshToken();
+
+        return refreshTokenService.findByToken(requestRefreshToken)
+                .map(refreshTokenService::verifyExpiration)
+                .map(RefreshToken::getUser)
+                .map(user -> {
+                    String token = jwtUtils.generateJwtToken(user.getLogin());
+                    return ResponseEntity.ok(new AuthResponse(token, requestRefreshToken));
+                })
+                .orElseThrow(() -> new TokenRefreshException(requestRefreshToken,
+                        "Refresh token is not in database!"));
     }
 
 }
